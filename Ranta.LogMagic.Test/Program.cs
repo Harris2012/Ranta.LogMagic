25234a6423cdbec5a0b00e182e66fb4eb63aadaa
HarrisZhang <harriszhang@live.cn>
2016-07-05 08:18:15 +0800

    update

---
+++ Ranta.LogMagic.Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranta.LogMagic.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger<Program>();
+
+            logger.Debug("this is debug.");
+            logger.Info("This is info.");
+            logger.Warn("This is warn.");
+            logger.Error("This is error.");
+            logger.Fault("This is fault.");
+
+            logger.Debug("this is debug.", "no more detail.");
+            logger.Info("This is info.", "no more detail.");
+            logger.Warn("This is warn.", "no more detail.");
+            logger.Error("This is error.", "no more detail.");
+            logger.Fault("This is fault.", "no more detail.");
+        }
+    }
+}
